3e41a0d5c93296ee5e14a2471e5cbf92200163df
Will Lehman <william.p.lehman@usace.army.mil>
2021-04-27 13:24:38 -0400

    a start at connecting to the NSI

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/HydrologicEngineeringCenter/go-coastal
+
+go 1.15
+
+require github.com/USACE/go-consequences v0.0.0-20210417021547-67fe47714343
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/USACE/go-consequences/consequences"
+	"github.com/USACE/go-consequences/structureprovider"
+)
 
 func main() {
+	nsp := structureprovider.InitNSISP()
+
 	fmt.Println("Hello World.")
+	fmt.Println("FIPS Code is " + "12")
+	var index int64 = 0
+	nsp.ByFips("12", func(f consequences.Receptor) {
+		index++
+	})
+	fmt.Println("Found " + strconv.FormatInt(index, 10) + " structures.")
 }
