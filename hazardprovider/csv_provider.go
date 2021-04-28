3aafe6d2846e3cac2b15e27fd11256c91ecb29c2
Will Lehman <william.p.lehman@usace.army.mil>
2021-04-27 22:26:47 -0400

    adding comments to describe data better

--- hazardprovider/csv_provider.go
+++ hazardprovider/csv_provider.go
@@ -32,6 +32,16 @@ func (csv csvHazardProvider) Close() {
 func (csv csvHazardProvider) ProvideHazard(l geography.Location) (hazards.HazardEvent, error) {
 	/*
 		This is where the hard part lives...
+		//raw data is in meters - unknown projection atm.
+		//There is no header, first row is real data.
+		//delimiter is comma
+		//lon,lat is not dependable, negative values should indicate lon...
+		//lon,lat,elevation(meters),.5,.2,.1,.05,.02,.01,.005,.002,.001,.0002,.0001 (all depths in meter, still water level)
+		//some xy locations have all zeros for depths.
+		//xy location represents nodes on a triangular irregular mesh.
+		//BE represents Best Estimate, CL90 90% exceedacnce value, SLC0 1996, SLC1 mid future condition, SLC2 high future condition
+		//data organized by state.
+
 	*/
 	h := hazards.CoastalEvent{}
 	h.SetDepth(123.45) //update from the actual file
