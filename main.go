66deb87d9803653d67e31ee87622af1d2d750a1c
Will Lehman <william.p.lehman@usace.army.mil>
2021-11-13 22:32:05 -0500

    updating to the change in namespace in go consequences for results writing

--- main.go
+++ main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/HydrologicEngineeringCenter/go-coastal/compute"
 )
 
 func main() {
-	argsWithoutProg := os.Args[1:]
-	if len(argsWithoutProg) != 2 {
-		fmt.Println("Expected two arguments, the filepath to the csv input and the file path to the geopackage input")
-	} else {
-		hfp := argsWithoutProg[0]
-		sfp := argsWithoutProg[1]
-		fmt.Println(fmt.Sprintf("Computing EAD for %v using an iventory at path %v", hfp, sfp))
-		compute.ExpectedAnnualDamagesGPK(hfp, sfp)
-	}
+	fmt.Println("hello world")
 
 }
