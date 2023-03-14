9a719b20f3547a10ddddbbef800434ad5cb61c9a
Will Lehman <william.p.lehman@usace.army.mil>
2023-03-14 08:33:38 -0400

    adding in the disaster report from fema

--- hazardprovider/chrps_provider_test.go
+++ hazardprovider/chrps_provider_test.go
@@ -53,3 +53,36 @@ func Test_CHRPS_Compute(t *testing.T) {
 	})
 	fmt.Println(hp)
 }
+func Test_CHRPS_Compute_ECAM(t *testing.T) {
+	root := "/workspaces/go-coastal/data/2008_GUSTAV"
+	fp := "/workspaces/go-coastal/data/cpra_2023updates_v14a_chk.grd"
+	hp, err := InitCHRPS(fp, "/workspaces/go-coastal/data/2008_GUSTAV_Adv_20_PredNodes.txt")
+	if err != nil {
+		panic(err)
+	}
+	nsp, err := structureprovider.InitGPK("/workspaces/go-coastal/data/nsi.gpkg", "nsi")
+	if err != nil {
+		panic(err)
+	}
+	w := resultswriters.InitDisasterOutput(root+"_disaster_report.csv", nsp)
+	defer w.Close()
+	fmt.Println("Getting bbox")
+	bbox, err := hp.ProvideHazardBoundary()
+	if err != nil {
+		log.Panicf("Unable to get the raster bounding box: %s", err)
+	}
+	fmt.Println(bbox.ToString())
+	//get FilterStructures
+	nsp.ByBbox(bbox, func(f consequences.Receptor) {
+		//ProvideHazard works off of a geography.Location
+		d, err2 := hp.ProvideHazard(geography.Location{X: f.Location().X, Y: f.Location().Y})
+		//compute damages based on hazard being able to provide depth
+		if err2 == nil {
+			r, err := f.Compute(d)
+			if err == nil {
+				w.Write(r)
+			}
+		}
+	})
+	fmt.Println(hp)
+}
